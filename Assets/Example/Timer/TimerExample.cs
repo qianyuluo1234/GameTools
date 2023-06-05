438e58b2832886c9fed1fbe3cc154f9df1a02fd6
qianyuluo <839435066@qq.com>
2023-06-05 20:44:54 +0800

    添加计时器示例

---
+++ Assets/Example/Timer/TimerExample.cs
@@ -0,0 +1,85 @@
+﻿using GameTools;
+using UnityEngine;
+
+namespace Example.Timer
+{
+    public class TimerExample : MonoBehaviour
+    {
+        public GameObject cube;
+        public GameObject sphere;
+        public GameObject capsule;
+
+        private int _cubeTimerId;
+        private int _sphereTimerId;
+        private int _capsuleTimerId;
+
+        private bool _sphereTimerPause = false;
+        private bool _sphereTimerRunning = false;
+        private void Awake()
+        {
+            TimerManager.instance.Init();
+        }
+
+        private void Start()
+        {
+            //执行一次
+            _cubeTimerId = TimerManager.instance.AddTimerByMilliseconds(1000,
+                () => { cube.transform.rotation = cube.transform.rotation * Quaternion.AngleAxis(30f, Vector3.forward);},false);
+            //执行无限次,不自动移除
+            _sphereTimerId = TimerManager.instance.AddTimerByMilliseconds(50,
+                () =>
+                {
+                    sphere.transform.position = sphere.transform.position + Vector3.right * Random.Range(-1f, 1f);
+                }, null, 0, false);
+            //执行n次,自动移除
+            int tempCount = 0;
+            _capsuleTimerId = TimerManager.instance.AddTimerBySeconds(0.1f,
+                () => { capsule.transform.position = capsule.transform.position + Vector3.forward * Random.Range(-1f, 1f);
+                    tempCount++; Debug.Log($"capsuleTimer runing count: {tempCount}"); }, null, 10);
+
+            
+            TimerManager.instance.Start(_cubeTimerId);
+            TimerManager.instance.Start(_sphereTimerId);
+            TimerManager.instance.Start(_capsuleTimerId);
+        }
+
+
+        private void Update()
+        {
+            TimerManager.instance.Tick();
+
+            if (Input.GetKeyDown(KeyCode.P))
+            {
+                _sphereTimerPause = !_sphereTimerPause;
+                TimerManager.instance.Pause(_sphereTimerId, _sphereTimerPause);
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                _sphereTimerRunning = !_sphereTimerRunning;
+                
+                if(_sphereTimerRunning) 
+                    TimerManager.instance.Stop(_sphereTimerId);
+                else
+                    TimerManager.instance.Restart(_sphereTimerId);
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                TimerManager.instance.Restart(_cubeTimerId);
+            }
+
+            if (Input.GetKeyDown(KeyCode.Alpha2))
+            {
+                TimerManager.instance.Restart(_capsuleTimerId);
+                Debug.Log("capsult timer autoDestroy == true");
+            }
+
+        }
+
+        private void OnDestroy()
+        {
+            TimerManager.instance.Clear();
+        }
+    }
+}
--- Assets/GameTimer/Scripts/TimerState.cs
+++ Assets/GameTimer/Scripts/TimerState.cs
@@ -1,19 +1,24 @@
-﻿public enum TimerState
+﻿namespace GameTools
 {
-    /// <summary>
-    /// new或Reset的状态
-    /// 此时计时器未初始化，各项参数均为设置或已清空
-    /// </summary>
-    Ready = 0,
-    /// <summary>
-    /// 初始化
-    /// 该状态下计时器所需参数已设置完毕
-    /// </summary>
-    Init,
-    /// <summary>
-    /// 运行状态
-    /// </summary>
-    Running,
-    Pause,
-    Stop
+    public enum TimerState
+    {
+        /// <summary>
+        /// new或Reset的状态
+        /// 此时计时器未初始化，各项参数均未设置或已清空
+        /// </summary>
+        Ready = 0,
+
+        /// <summary>
+        /// 初始化
+        /// 该状态下计时器所需参数已设置完毕
+        /// </summary>
+        Init,
+
+        /// <summary>
+        /// 运行状态
+        /// </summary>
+        Running,
+        Pause,
+        Stop
+    }
 }
